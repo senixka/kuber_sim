b5fe9e207acf0022c2ccbe75323eaeb0315f5430
Arseniy Karnaukhov <senixka@gmail.com>
2024-05-07 21:58:59 +0300

    New examples with different load types

---
+++ examples/pod_group_removal/src/main.rs
@@ -0,0 +1,42 @@
+use kuber_sim::my_imports::*;
+
+/// This example shows how group_duration works for pod groups.
+/// When duration expires, all pods of the corresponding group are removed.
+fn main() {
+    // Read input
+    let mut init_config = InitConfig::from_yaml(&"./in_pod_group_removal.yaml".to_string());
+    let mut init_nodes = InitNodes::from_yaml(&"./in_pod_group_removal.yaml".to_string());
+    let mut init_trace = InitTrace::from_file(&"./in_pod_group_removal.yaml".to_string());
+
+    // Prepare input
+    init_config.prepare();
+    init_nodes.prepare();
+    init_trace.prepare();
+
+    // Prepare scheduler pipeline config
+    let pipeline_config = PipelineConfig::new(
+        Box::new(ActiveQDefault::default()),
+        Box::new(BackOffQConstant::new(1.0)),
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+        vec![],
+    );
+
+    // Create simulation
+    let mut sim = Simulation::new(
+        "./out_pod_group_removal.txt".to_string(),
+        &init_config,
+        &init_nodes,
+        &init_trace,
+        &pipeline_config,
+        123,
+        false,
+        false,
+        false,
+    );
+
+    // Work with simulation
+    sim.step_for_duration(50.0);
+}
