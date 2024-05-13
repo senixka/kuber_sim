83d6ef2ecb168499a0704e0229b80f379f249d5b
Arseniy Karnaukhov <senixka@gmail.com>
2024-05-13 23:52:07 +0300

    Updated generator and converter formats

--- google_cluster_log/converter.py
+++ google_cluster_log/converter.py
@@ -1,49 +1,37 @@
 def dump_yaml_pod_group(fout, arrival_time, pod_count, duration, cpu, memory):
     s = f"""
-  - pod_group:
-    amount: {pod_count}
-    pod:
-      spec:
-        arrival_time: {arrival_time}
-        request_cpu: {cpu}
-        request_memory: {memory}
-        load:
-          !Constant
-          cpu: {cpu}
-          memory: {memory}
-          duration: {duration}"""
+  - submit_time: {arrival_time}
+    event:
+      !AddPodGroup
+      pod_count: {pod_count}
+      pod:
+        spec:
+          request_cpu: {cpu}
+          request_memory: {memory}
+          limit_cpu: {memory}
+          limit_memory: {memory}
+          load:
+            !Constant
+            cpu: {cpu}
+            memory: {memory}
+            duration: {duration}
+"""
     fout.write(s)
 
 
 def dump_csv_pod_group(fout, arrival_time, pod_count, duration, cpu, memory):
-    s = f"{pod_count},{arrival_time},{cpu},{memory},,,,,,constant;{cpu};{memory};{duration}\n"
+    s = f"{arrival_time};0;{pod_count};;{{{{}};{{{cpu};{memory};{cpu};{memory};;{{0;{cpu};{memory};{duration}}};{{}};{{}};{{}}}}}};{{}};{{}}\n"
     fout.write(s)
 
 
 def dump_state_prolog(fout):
     s = f"""
-network_delays:
-  api2scheduler: 0
-  scheduler2api: 0
-  api2kubelet: 1
-  kubelet2api: 1
-  api2ca: 0
-  ca2api: 0
+monitoring:
+  self_update_period: 1
 
-constants:
-  kubelet_self_update_period: 100000
-  scheduler_self_update_period: 0.5
-  monitoring_self_update_period: 10
-  scheduler_cycle_max_scheduled: 0
-  scheduler_cycle_max_to_try: 0
-  unschedulable_queue_period: 30
-  ca_self_update_period: 10
-  ca_add_node_delay_time: 10
-  ca_add_node_min_pending: 0
-  ca_remove_node_cpu_percent: 15
-  ca_remove_node_memory_percent: 15
-  ca_remove_node_delay_cycle: 3
-  hpa_self_update_period: 10
+scheduler:
+  self_update_period: 1
+  unschedulable_queue_backoff_delay: 5
 
 """
     fout.write(s)
@@ -65,27 +53,7 @@ def main():
     if s_ext not in ["yaml", "csv"]:
         exit(1)
 
-    with open(f"../data/workload/test_g{s_ext}.{s_ext}", 'w') as fout:
-        if s_ext == "yaml":
-            fout.write("pods:")
-
-        with open("job_and_task.txt", 'r') as fin:
-            n_job, n_task = map(int, fin.readline().split())
-
-            for i in range(0, int(n_job)):
-                fin.readline()
-                arrival_time, group_count = map(int, fin.readline().split())
-
-                for j in range(0, group_count):
-                    task_count, duration, cpu, memory = map(int, fin.readline().split())
-                    if s_ext == "yaml":
-                        dump_yaml_pod_group(fout, arrival_time, task_count, duration, cpu, memory)
-                    elif s_ext == "csv":
-                        dump_csv_pod_group(fout, arrival_time, task_count, duration, cpu, memory)
-                if i % 10000 == 0:
-                    print(f"Done job: {i}/{n_job}")
-
-    with open("../data/cluster_state/test_gcsv.yaml", 'w') as fout:
+    with open("in_config.yaml", 'w') as fout:
         dump_state_prolog(fout)
         fout.write("nodes:")
 
@@ -95,6 +63,28 @@ def main():
             for i in range(0, n_group):
                 machine_count, cpu, memory = map(int, fin.readline().split())
                 dump_machine_group(fout, machine_count, cpu, memory)
+
+    with open(f"in_trace.{s_ext}", 'w') as fout:
+        if s_ext == "yaml":
+            fout.write("trace:")
+
+        with open("job_and_task.txt", 'r') as fin:
+            n_job, n_task = map(int, fin.readline().split())
+
+            for i in range(0, int(n_job)):
+                fin.readline()
+                arrival_time, group_count = map(int, fin.readline().split())
+
+                for _ in range(0, group_count):
+                    task_count, duration, cpu, memory = map(int, fin.readline().split())
+                    if duration == 4294967295:
+                        duration = 60 * 60 * 24 * 29
+                    if s_ext == "yaml":
+                        dump_yaml_pod_group(fout, arrival_time, task_count, duration, cpu, memory)
+                    elif s_ext == "csv":
+                        dump_csv_pod_group(fout, arrival_time, task_count, duration, cpu, memory)
+                if i % 50000 == 0:
+                    print(f"Done job: {i}/{n_job}")
 
 
 if __name__ == "__main__":
